69dbcd91188fdb8e191c5c5ce534e649f5222afd
YSLee-Dev <ysleedev01@gmail.com>
2022-01-24 18:23:50 +0900

    프로젝트 생성

---
+++ 8_pomodoro_timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  8_pomodoro_timer
+//
+//  Created by 이윤수 on 2022/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        self.view.backgroundColor = .white
+    }
+
+
+}
+
+
